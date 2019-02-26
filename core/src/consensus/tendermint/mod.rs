dcc5203470083788dc5644ca3ead45666e4121cd
Juhyung Park <jh@kodebox.io>
2019-02-26 20:09:19 +0900

    When the height is changed, the step should be the Propose step
    In the past, since the Commit state is the first state of the height,
    the step is changed to the Commit step after a block is imported.
    
    However, in the current, the Commit step is the last step of the
    height.
    
    This patch fixes the unchanged code in the past.

--- core/src/consensus/tendermint/mod.rs
+++ core/src/consensus/tendermint/mod.rs
@@ -1371,7 +1371,7 @@ impl ChainNotify for TendermintChainNotify {
                 }
             }
             if height_changed {
-                t.move_to_step(Step::Commit, false);
+                t.move_to_step(Step::Propose, false);
                 return
             }
         }
